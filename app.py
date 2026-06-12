115dc1a9c90bdfbb7dfefed55958d14a987fccd8
Naveen <nbamrara0@gmail.com>
2026-06-12 12:28:18 +0530

    update app.py

--- app.py
+++ app.py
@@ -13,9 +13,9 @@ ADMIN_USERNAME = os.getenv('ADMIN_USERNAME', 'admin')
 ADMIN_PASSWORD = os.getenv('ADMIN_PASSWORD', 'mysecretpassword123')
 
 # Database Setup
-database_url = os.getenv("DATABASE_URL")
+# database_url = os.getenv("DATABASE_URL")
 
-app.config['SQLALCHEMY_DATABASE_URI'] = database_url
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://bamrara:8Qva8loQ9FZfXeCp4m28vtphSzBGM0cL@dpg-d8836pgg4nts73eogpmg-a.oregon-postgres.render.com/house_db_bqk1"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config["SQLALCHEMY_ENGINE_OPTIONS"] = {
     "connect_args": {"sslmode": "require"}
